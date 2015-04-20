d0719992311a0b699524c28edd58332706fef32f
Amith Gotamey <kishanterry@gmail.com>
2015-04-20 19:05:35 +0600

    fixed query string

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,11 +1,13 @@
 <?php namespace App\Http\Controllers;
 
+use Illuminate\Support\Str;
 use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
     public function __construct()
     {
-
+        $hash = Str::random(6);
+        dump($hash);
     }
 }
--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -13,20 +13,9 @@ class HomeController extends Controller
     public function index(Request $request, $hash = null)
     {
         $requests = $request->all();
-        $redirect = null;
-        if ($requests) {
-            $requests = collect($requests);
-
-            $url_key = $requests->keys()->first() ? str_replace('_', '.', $requests->keys()->first()) : null;
-            $url_value = $requests->first();
-
-            if ($this->validateURL($url_key)) {
-                $redirect = $url_key;
-            } elseif ($this->validateURL($url_value)) {
-                $redirect = $url_value;
-            }
-        }
-        if ($redirect) {
+        if ($request->getQueryString()) {
+            $redirect = urldecode($request->getQueryString());
+//            dump($redirect);
             return view('redirect', compact('redirect'));
         }
 
