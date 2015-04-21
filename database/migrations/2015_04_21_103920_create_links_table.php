3e9e92af2fa5cd6a8fffd8df1036e3e95d495747
Amith Gotamey <kishanterry@gmail.com>
2015-04-21 16:42:12 +0600

    created links migration and model

---
+++ database/migrations/2015_04_21_103920_create_links_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLinksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('links', function(Blueprint $table) {
+			$table->bigIncrements('id')->unsigned();
+			$table->string('url');
+			$table->string('hash');
+			$table->integer('created_by')->unsigned();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('links');
+	}
+
+}
