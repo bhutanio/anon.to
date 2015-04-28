06db6539e081eca14b0666e821ff11b3f2b331a5
Abi Narayan <abixalmon@gmail.com>
2015-04-28 19:56:30 +0600

    clean up css

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Anon.to</title>
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Montserrat:400,700">
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
+    <title>Anon.to - Anonymous URL Shortener</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic">
     <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}"/>
 </head>
 <body id="page-top">
@@ -29,7 +29,7 @@
                     <a href="#page-top"></a>
                 </li>
                 <li class="page-scroll">
-                    <a href="#about">About</a>
+                    <a href="#redirect">Redirect</a>
                 </li>
             </ul>
         </div>
@@ -41,7 +41,7 @@
         <div class="row">
             <div class="col-lg-12">
                 <div class="intro-text">
-                    <span class="name">Anonymous URL Shortener</span>
+                    <h1 class="name">Anonymous URL Shortener</h1>
                     <hr class="star-light">
                     <span class="skills">Paste a link to shorten it</span>
 
@@ -54,9 +54,9 @@
                     <form action="{{ route('shorten') }}" method="POST" class="form-inline">
                         <div class="form-group">
                             <label class="sr-only" for="exampleInputEmail3">URL</label>
-                            <input type="url" name="url" class="form-control col-sm-6" id="url" placeholder="Enter URL">
+                            <input type="url" name="url" class="form-control" id="url" placeholder="Enter URL">
                         </div>
-                        <button type="submit" class="btn btn-default">Shorten</button>
+                        <button type="submit" class="btn btn-info">Shorten</button>
                     </form>
 
                 </div>
@@ -65,16 +65,52 @@
     </div>
 </header>
 
-<!-- Contact Section -->
-<section id="about">
+<section id="redirect">
     <div class="container">
         <div class="row">
+            <div class="col-sm-12 text-center">
+                <h2>Anonymous Redirect</h2>
+                <hr class="star-primary">
+            </div>
             <div class="col-sm-12">
-                @yield('content')
+                <p>Do you want to link anonymously to other web sites without sending any referrer?</p>
+                <p>Then use <strong>anon.to</strong></p>
+                <p>Just put <strong>https://anon.to/?</strong> in front of your links.</p>
+                <p>Example link: <strong>https://anon.to/?http://www.google.com</strong></p>
             </div>
         </div>
     </div>
 </section>
+
+<footer class="text-center">
+    <div class="footer-above">
+        <div class="container">
+            <div class="row">
+                <div class="footer-col col-md-6">
+                    <h3>Contribute</h3>
+                    <ul class="list-inline">
+                        <li>
+                            <a href="https://github.com/bhutanio/anon.to" class="btn-social btn-outline"><i class="fa fa-fw fa-github"></i></a>
+                        </li>
+                    </ul>
+                </div>
+                <div class="footer-col col-md-6">
+                    <h3>About Anon.to</h3>
+                    <p>Anon.to is a free to use, open source URL Shortener and Redirector, created by <a href="http://bhutan.io">bhutan.io</a></p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer-below">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    Copyright &copy; Anon.to
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
 
 <script src="{{ asset('assets/js/app.js') }}"></script>
 </body>
--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -1,11 +1,44 @@
-<!doctype html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="refresh" content="0; url={{ $redirect }}">
     <title>Redirecting to {{ $redirect }}</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            height: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: Menlo, Monaco, Consolas, "Courier New", monospace;
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            font-size: 18px;
+            display: inline-block;
+        }
+
+        .content a {
+            color: #5bc0de;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
-    <h1>Redirecting to <a href="{{ $redirect }}" title="{{ $redirect }}" rel="noreferrer nofollow">{{ $redirect }}</a></h1>
+<div class="container">
+    <div class="content">
+        <h1>Redirecting to <a href="{{ $redirect }}" title="{{ $redirect }}" rel="noreferrer nofollow">{{ \Illuminate\Support\Str::limit($redirect,32) }}</a>
+        </h1>
+    </div>
+</div>
 </body>
 </html>
