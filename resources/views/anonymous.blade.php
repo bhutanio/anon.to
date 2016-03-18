55ca35f1bdd018c6cb9f349a95ae6406262547e1
Abi Narayan <abixalmon@gmail.com>
2016-03-18 18:31:37 +0600

    added GOOGLE ANALYTICS

--- resources/views/anonymous.blade.php
+++ resources/views/anonymous.blade.php
@@ -17,11 +17,14 @@
 </div>
 
 <script>
-    (function(){
-        if(window.location.hash) {
-            document.getElementsByName('url')[0].setAttribute('content', '0; url={{ $url }}' + window.location.hash);
-        }
-    })();
+(function(){
+    if(window.location.hash) {
+        document.getElementsByName('url')[0].setAttribute('content', '0; url={{ $url }}' + window.location.hash);
+    }
+})();
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{{ env('GOOGLE_ANALYTICS') }}', 'auto');
+ga('send', 'pageview');
 </script>
 
 </body>
