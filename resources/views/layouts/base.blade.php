4fdc5a77bdbaed32ca8f4ca381324097a0e7a094
Abi Narayan <abixalmon@gmail.com>
2016-03-18 18:27:33 +0600

    added GOOGLE ANALYTICS

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -34,5 +34,12 @@
 <script id="github-bjs" src="https://buttons.github.io/buttons.js"></script>
 <script src="{{ url('js/app.js?v='.asset_version()) }}"></script>
 @yield('footer_js')
+
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{{ env('GOOGLE_ANALYTICS') }}', 'auto');
+ga('send', 'pageview');
+</script>
+
 </body>
 </html>
