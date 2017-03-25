df82f4900bb691245a2ba609b0335c83ebad0426
Abi Narayan <abixalmon@gmail.com>
2017-03-26 00:39:01 +0600

    better exception handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,57 +3,44 @@
 namespace App\Exceptions;
 
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
+use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
-    /**
-     * A list of the exception types that should not be reported.
-     *
-     * @var array
-     */
+
     protected $dontReport = [
-        \Illuminate\Auth\AuthenticationException::class,
-        \Illuminate\Auth\Access\AuthorizationException::class,
-        \Symfony\Component\HttpKernel\Exception\HttpException::class,
-        \Illuminate\Database\Eloquent\ModelNotFoundException::class,
-        \Illuminate\Session\TokenMismatchException::class,
-        \Illuminate\Validation\ValidationException::class,
+        AuthenticationException::class,
+        AuthorizationException::class,
+        HttpException::class,
+        ModelNotFoundException::class,
+        TokenMismatchException::class,
+        ValidationException::class,
     ];
 
-    /**
-     * Report or log an exception.
-     *
-     * This is a great spot to send exceptions to Sentry, Bugsnag, etc.
-     *
-     * @param  \Exception  $exception
-     * @return void
-     */
     public function report(Exception $exception)
     {
         parent::report($exception);
     }
 
-    /**
-     * Render an exception into an HTTP response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Exception  $exception
-     * @return \Illuminate\Http\Response
-     */
     public function render($request, Exception $exception)
     {
+        if (!($exception instanceof ValidationException) && $request->expectsJson()) {
+            return $this->respondJsonException($request, $exception);
+        }
+
+        if ($exception instanceof TokenMismatchException) {
+            return $this->handleTokenMismatch($request, $exception);
+        }
+
         return parent::render($request, $exception);
     }
 
-    /**
-     * Convert an authentication exception into an unauthenticated response.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Illuminate\Auth\AuthenticationException  $exception
-     * @return \Illuminate\Http\Response
-     */
     protected function unauthenticated($request, AuthenticationException $exception)
     {
         if ($request->expectsJson()) {
@@ -61,5 +48,29 @@ class Handler extends ExceptionHandler
         }
 
         return redirect()->guest(route('login'));
+    }
+
+    private function respondJsonException($request, $exception)
+    {
+        if (method_exists($exception, 'getStatusCode')) {
+            return response()->json($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof AuthorizationException) {
+            return response()->json('Unauthorized Action', 403);
+        }
+
+        return response()->json('Internal Server Error', 500);
+    }
+
+    private function handleTokenMismatch($request, TokenMismatchException $exception)
+    {
+        if ($request->expectsJson()) {
+            return response()->json('Your session has expired. Please try again.', 401);
+        }
+
+        flash('Your session has expired. Please try again.', 'warning');
+
+        return redirect()->back()->withInput($request->except('_token'));
     }
 }
